235071e524de66bac0ab313d1d17cb4af6e7e327
meysam <meisam.design.ph@gmail.com>
2021-10-19 22:46:44 +0330

    improve code

---
+++ index.js
@@ -0,0 +1,17 @@
+const slider = document.querySelectorAll(".slider");
+const next = document.querySelectorAll(".next");
+const prev = document.querySelectorAll(".prev");
+for (let i = 0; i < next.length; i++) {
+  next[i].addEventListener("click", function(){
+    for (let i = 0; i < slider.length; i++) {
+      slider[i].style.transform = "translateX(-120rem)"
+    }
+  })
+}
+for (let i = 0; i < next.length; i++) {
+  prev[i].addEventListener("click", function(){
+    for (let i = 0; i < slider.length; i++) {
+      slider[i].style.transform = "translateX(0)"
+    }
+  })
+}
