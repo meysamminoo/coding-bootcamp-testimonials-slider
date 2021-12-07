031573b53c56ec4c63eaeb1825a389f06c9d6880
meysam <meisam.design.ph@gmail.com>
2021-12-08 00:09:40 +0330

    complate

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const slider = document.querySelectorAll(".slider");
 const next = document.querySelectorAll(".next");
 const prev = document.querySelectorAll(".prev");
 for (let i = 0; i < next.length; i++) {
-  next[i].addEventListener("click", function(){
+  next[i].addEventListener("click", function () {
     for (let i = 0; i < slider.length; i++) {
-      slider[i].style.transform = "translateX(-120rem)"
+      slider[i].style.transform = "translateX(-100vw)";
     }
-  })
+  });
 }
 for (let i = 0; i < next.length; i++) {
-  prev[i].addEventListener("click", function(){
+  prev[i].addEventListener("click", function () {
     for (let i = 0; i < slider.length; i++) {
-      slider[i].style.transform = "translateX(0)"
+      slider[i].style.transform = "translateX(0)";
     }
-  })
+  });
 }
